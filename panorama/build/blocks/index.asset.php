bc7ef047093cf7bea7c5d0409762e28263990ce6
Sean Murphy <sean@iamseanmurphy.com>
2026-06-14 11:11:16 +0000

    1.7.2

--- panorama/build/blocks/index.asset.php
+++ panorama/build/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5659e5cd1dc971f72db3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '88a7594df95df3025389');
